1d68f2a35bd7591cd0cb958ffd679da6a6111db7
Ivan G <ivan.grsko@gmail.com>
2026-08-31 21:02:01 +1200

    added simple logging draft

---
+++ src/01-glossary-comments-logs/20-logs.ts
@@ -0,0 +1,14 @@
+/*
+ * TODO:
+ * Logging
+ *
+ * Simple way to show what is going on in your program to the user
+ */
+
+//
+console.log('Hello World!');
+
+//
+const world = "World";
+console.log('Hello', world);
+console.log(`Hello ${world}!`);

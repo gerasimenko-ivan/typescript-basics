0a9a5850ee6bb54cb60a68840f565ab1912be161
Ivan G <ivan.grsko@gmail.com>
2026-09-04 10:18:25 +1200

    add draft about ts js type extension, differences

---
+++ src/03-data-types/10-basics/90-ts-types-extension-to-js.ts
@@ -0,0 +1,13 @@
+/* TODO: -- info about differences between TS and JS,
+    especially in types, because it's TYPE-script
+object
+array (string[], number[])
+tuple
+enum
+any
+unknown
+void
+never
+union (string | number)
+intersection (A & B)
+ */

e28b52936a2cbf6e50cc028bc5dd0ca2c33a0f2d
Ivan G <ivan.grsko@gmail.com>
2026-09-05 18:32:44 +1200

    update array definition, add examples of accessing elements by index

---
+++ src/03-data-types/40-arrays/10-arrays.ts
@@ -0,0 +1,26 @@
+/*
+ * Arrays
+ *
+ * An array is an ordered collection of values, usually of the same type.
+ * These values are stored in one variable.
+ *
+ * There are some specific tools to work with these values:
+ *  - access an element by index (index starts from 0)
+ *  - add and remove elements
+ *  - find, replace, sort, filter, etc.
+ */
+
+// cars       -- name of an array;
+// : string[] -- type of array
+// = [];      -- initialization of array with empty array
+const cars: string[] = [];
+console.log('cars', cars);
+
+
+// TypeScript infers the type as string[] from the values in the array (string values).
+const cars2 = ['Toyota', 'Ford', 'Ferrari'];
+
+console.log('cars2', cars2);       // whole array
+console.log('cars2[0]', cars2[0]); // access 'Toyota' element by index
+console.log('cars2[1]', cars2[1]); // access 'Ford' element by index
+console.log('cars2[2]', cars2[2]); // access 'Ferrari' element by index

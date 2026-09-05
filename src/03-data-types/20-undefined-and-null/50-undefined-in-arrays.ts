df2f87464280720c3c439999d6bbe2eb02764f8f
Ivan G <ivan.grsko@gmail.com>
2026-09-05 19:22:27 +1200

    add example of undefined returned by array.pop()

---
+++ src/03-data-types/20-undefined-and-null/50-undefined-in-arrays.ts
@@ -0,0 +1,32 @@
+export {}
+/*
+ * Undefined and Array
+ *
+ * The pop() function removes and returns the last element.
+ * If pop() is called on an empty array, it returns undefined.
+ */
+
+const cars = ['Toyota', 'Ford'];
+
+/* type of lastElement is 'string | undefined'
+   because if pop() is called on an empty array, it will return undefined */
+let lastElement = cars.pop();
+console.log('lastElement', lastElement); // lastElement Ford
+
+/* line below will cause an error:
+   "Type string | undefined is not assignable to type string" */
+// let lastElementOnlyString: string = cars.pop(); // ❌ not allowed
+
+
+lastElement = cars.pop();
+console.log('lastElement', lastElement); // lastElement 'Toyota'
+
+lastElement = cars.pop();
+console.log('lastElement', lastElement); // lastElement 'undefined'
+
+
+/*
+ * See also:
+ * - src/03-data-types/40-arrays/11-array-add-remove-element.ts
+ *    └──  Array.prototype.pop()
+ */

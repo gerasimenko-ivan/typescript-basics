d7e57bdb76cfa5c5ba1d63612245fd03605c040f
Ivan G <ivan.grsko@gmail.com>
2026-09-07 10:46:02 +1200

    add array.length property demonstration

---
+++ src/03-data-types/40-arrays/11-array-length-property.ts
@@ -0,0 +1,22 @@
+export {}
+/*
+ * Array - Length Property
+ *
+ * .length - returns current length of an array (number of elements in an array)
+ */
+
+const cars = ['Toyota', 'Ford', 'Ferrari', 'Audi'];
+
+console.log(`cars.length = ${cars.length}`); // length 4
+
+// if we remove one element, the length decreases
+cars.pop(); // remove one element
+console.log(cars);
+console.log(`cars.length = ${cars.length}`); // length 3
+
+
+/*
+ * .pop() - method
+ * See also:
+ * - src/03-data-types/40-arrays/21-array-add-remove-element.ts
+ */

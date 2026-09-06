729a8721d00a0fdd9eed1a1868678973e2842aa4
Ivan G <ivan.grsko@gmail.com>
2026-09-06 20:59:25 +1200

    add example of updating an element of an array

---
+++ src/03-data-types/40-arrays/11-array-update-element.ts
@@ -0,0 +1,15 @@
+export {}
+/*
+ * Array - Update an element
+ */
+
+const cars = ['Toyota', 'Ford', 'Ferrari'];
+
+
+// We can access element by index:
+console.log(cars[1]); // output: Ford
+
+
+// We can also update element by index:
+cars[1] = 'Audi';
+console.log(cars);    // output: [ 'Toyota', 'Audi', 'Ferrari' ]

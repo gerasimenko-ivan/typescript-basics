1c8438c322fb1f07cfe844bae88f5cf1de8adfbc
Ivan G <ivan.grsko@gmail.com>
2026-09-08 19:44:27 +1200

    add an example of class

---
+++ src/03-data-types/91-classes/10-class.ts
@@ -0,0 +1,34 @@
+export {}
+/*
+ * Class
+ *
+ * A class is a definition for creating objects with
+ * a particular structure and behaviour.
+ *
+ * Class is similar to Object in some ways:
+ *  - it can have properties, methods
+ *  - its name can be used as a type
+ *
+ * A class also provides additional features:
+ *  - it can create objects using 'new'
+ *  - it can have constructors
+ *  - it can use inheritance
+ *  - it can define access modifiers such as private and protected
+ *  - ...
+ */
+
+class Country {
+    name: string | undefined;
+    capital: string | undefined;
+}
+
+// Using class Country as a type
+const country1: Country = { name: 'Australia', capital: 'Canberra' };
+
+// Creating an instance of class Country -- only possible with class
+const country2: Country = new Country();
+
+/*
+ * The advantages of creating an object with 'new Country()'
+ * will become clearer when we introduce the class constructor.
+ */

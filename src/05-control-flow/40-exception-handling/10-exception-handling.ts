d53c753f686068f4c62686bae004d9b9de3ffca9
Ivan G <ivan.grsko@gmail.com>
2026-09-10 13:11:35 +1200

    add basic example of exception handling

---
+++ src/05-control-flow/40-exception-handling/10-exception-handling.ts
@@ -0,0 +1,30 @@
+export {}
+/*
+ * Exception Handling
+ *
+ * 'throw new Error()' is used to throw an error.
+ *
+ * 'try...catch' is used to catch and handle errors.
+ *
+ * If an error is thrown inside the 'try' block,
+ * execution of the 'try' block stops and the 'catch' block is executed.
+ */
+
+
+function printLine(line: string) {
+    if (line.length === 0) {
+        throw new Error('Line is empty');
+    }
+    console.log(line);
+}
+
+function tryPrintLine(line: string) {
+    try {
+        printLine(line);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+tryPrintLine('Hello World');
+tryPrintLine('');
